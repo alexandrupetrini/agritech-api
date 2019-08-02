1e1c9bdd0fc1e2e4d2c0c126401a74fa6e4bf854
Alex Petrin <cyberalex4life@gmail.com>
2019-08-02 15:14:33 +0300

    Added config.js file

---
+++ config.js
@@ -0,0 +1,13 @@
+const databaseOptions = {
+    host: "localhost",
+    database: "agritech_ttn",
+    user: "agritech",
+    password: "agritech"
+};
+
+const TTNOptions = {
+    appID: "digital-nation-use-case-1",
+    accessKey: "ttn-account-v2.xNqpGaPSJqrLo8sujwTmyceO6ZXbaDUgjPbxeUA2zB8"
+};
+
+module.exports = { databaseOptions: databaseOptions, TTNOptions: TTNOptions };
