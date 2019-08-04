9a90b50178e27e9e51de107c317f53efdb03b471
Alex Petrin <cyberalex4life@gmail.com>
2019-08-04 15:21:58 +0300

    Refactored query string building and the data to returned

--- model/tableModel.js
+++ model/tableModel.js
@@ -17,8 +17,7 @@ Table.echo_data = function (result) {
 };
 
 Table.getSensorsData = function (result) {
-    let query = "SELECT ? FROM ?";
-    query = "SELECT " + table.collumns.toString() + " FROM " + table.name;
+    let query = "SELECT " + table.collumns.toString() + " FROM " + table.name;
 
     sql.query(query, function (err, rows) {
         if (err) {
@@ -29,13 +28,14 @@ Table.getSensorsData = function (result) {
             let data = [];
             console.log("Data retrieved from table 'sensor_data'");
             rows.forEach(row => {
-                // let time = moment(row.time).format("hh:mma");
+                let date = moment(row.time).format("YYYY-MM-DD");
                 let hours = moment(row.time).format("hh");
                 let minutes = moment(row.time).format("mm");
                 let time_only_in_hours = parseInt(hours, 10) + parseInt(minutes, 10) / 60;
                 time_only_in_hours += "PM";
                 data.push({
-                    name: time_only_in_hours,
+                    Data: date,
+                    Ora: time_only_in_hours,
                     Temperatura: row.temperature,
                     Umiditate: row.humidity,
                 });
