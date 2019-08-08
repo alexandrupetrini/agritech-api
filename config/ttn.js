db298a23ac2992a264563bf4c76713e47f6b0c78
Alex Petrin <cyberalex4life@gmail.com>
2019-08-08 15:26:33 +0300

    Refactored ttn connection parameter declaration

--- config/ttn.js
+++ config/ttn.js
@@ -1,6 +1,6 @@
-const TTNOptions = {
+const ttnConnection = {
     appID: "digital-nation-use-case-1",
     accessKey: "ttn-account-v2.xNqpGaPSJqrLo8sujwTmyceO6ZXbaDUgjPbxeUA2zB8"
 };
 
-module.exports = TTNOptions;
+module.exports = ttnConnection;
