a3c63aa2514b887684707d5b4a496e65048b0817
Alex Petrin <cyberalex4life@gmail.com>
2019-08-10 15:17:44 +0300

    'storeTTNData' becomes 'ttnController'; refactored 'app.js' to use the 'ttnContoller'

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-var ttnData = require('./storage/storeTTNData');
+var ttn = require('./controllers/ttnController').store();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,8 +16,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-ttnData.store();
 
 app.use(cors());
 app.use(logger('dev'));
@@ -32,12 +30,12 @@ app.use('/testAPI', testAPIRouter);
 app.use('/sensorsData', sensorsDataRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ controllers/ttnController.js
@@ -0,0 +1,91 @@
+var ttn = require('ttn');
+var dbConnection = require('../config/db');
+var ttnConnection = require('../config/ttn');
+
+var TTN = function() {};
+
+TTN.store = function() {
+  ttn
+    .data(ttnConnection.appID, ttnConnection.accessKey)
+    .then(function(client) {
+      console.log('Expecting uplink from app', ttnConnection.appID);
+      client.on('uplink', async function(devID, payload) {
+        console.log('Received uplink from', devID);
+
+        if (payload.counter != undefined) {
+          console.log(payload);
+
+          var hardware_serial = payload.hardware_serial;
+          var port = payload.port;
+          var counter = payload.counter;
+          var payload_raw = payload.payload_raw;
+
+          // Do not convert the date time to local timezone.
+          // payload.metadata.time: The timezone is zero UTC offset
+          // Keep it in UTC format, users can convert the date time to their local time zone.
+          //
+          // The Things Network, the time is measured with 9 digits fractional-seconds, example: '2018-12-27T14:39:12.420921047Z'
+          // There are two issues.
+          // - Moment.js is a wrapper for the Date object in JavaScript, and is limited to three decimal places (milliseconds).
+          //   This is because that is all that the date object supports.
+          // - MySQL 5.7 has fractional seconds support for DATETIME with up to microseconds (6 digits) precision:
+          //   See: https://dev.mysql.com/doc/refman/5.7/en/fractional-seconds.html
+          //
+          // Using this solution will lose some microseconds precision:
+          // const time = moment(payload.metadata.time).utc().format('YYYY-MM-DD HH:mm:ss.SSSSSS');
+          // Because of this I have decided to store the time as a VARCHAR and NOT as a DATETIME(6)
+          //
+
+          var time = payload.metadata.time;
+          var frequency = payload.metadata.frequency;
+          var modulation = payload.metadata.modulation;
+          var data_rate = payload.metadata.data_rate;
+          var airtime = payload.metadata.airtime;
+          var coding_rate = payload.metadata.coding_rate;
+          var gateways = JSON.stringify(payload.metadata.gateways);
+          var humidity = payload.payload_fields.humidity / 100;
+          var temperature = payload.payload_fields.temperature;
+
+          // console.log('hardware_serial=', hardware_serial);
+          // console.log('port=', port);
+          // console.log('counter=', counter);
+          // console.log('payload_raw=', payload_raw);
+          // console.log('time=', time);
+          // console.log('frequency=', frequency);
+          // console.log('modulation=', modulation);
+          // console.log('data_rate=', data_rate);
+          // console.log('airtime=', airtime);
+          // console.log('coding_rate=', coding_rate);
+          // console.log('gateways=', gateways);
+
+          const query = 'INSERT INTO sensor_data SET ?';
+          const values = {
+            hardware_serial: hardware_serial,
+            port: port,
+            counter: counter,
+            payload_raw: payload_raw,
+            time: time,
+            frequency: frequency,
+            modulation: modulation,
+            data_rate: data_rate,
+            airtime: airtime,
+            coding_rate: coding_rate,
+            gateways: gateways,
+            humidity: humidity,
+            temperature: temperature
+          };
+
+          dbConnection.query(query, values, function(err, results) {
+            if (err) throw err;
+            console.log('Record inserted: ', results.insertId);
+          });
+        }
+      });
+    })
+    .catch(function(error) {
+      console.error('Error', error);
+      process.exit(1);
+    });
+};
+
+module.exports = TTN;
