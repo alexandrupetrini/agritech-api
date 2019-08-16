9163c82829a9af96b0cb53e3fb7b0ee20c29d1c2
Alex Petrin <cyberalex4life@gmail.com>
2019-08-16 16:11:34 +0300

    Fixed frequency and createdAt/updatedAt seed values

--- seeders/20190816120725-seed-sensors_data.js
+++ seeders/20190816120725-seed-sensors_data.js
@@ -21,7 +21,7 @@ module.exports = {
           counter: 99,
           payload_raw: ' 0',
           time: '2019-07-30T09:19:45.718185472Z',
-          frequency: 868100,
+          frequency: 868.1,
           modulation: 'LORA',
           data_rate: 'SF7BW125',
           airtime: 46336000,
@@ -30,8 +30,8 @@ module.exports = {
             '[{"gtw_id":"eui-30aea4fffe2cf768","timestamp":3496203105,"time":"2019-07-30T09:19:45.652395Z","channel":0,"rssi":-19,"snr":7,"rf_chain":0,"latitude":44.434933,"longitude":26.04587,"altitude":15,"location_source":"registry"}]',
           humidity: 0.48,
           temperature: 23,
-          createdAt: new Date().toDateString(),
-          updatedAt: new Date().toDateString()
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           hardware_serial: '70B3D5499243F193',
@@ -39,7 +39,7 @@ module.exports = {
           counter: 1,
           payload_raw: ' _',
           time: '2019-07-30T09:20:33.871020907Z',
-          frequency: 868100,
+          frequency: 868.1,
           modulation: 'LORA',
           data_rate: 'SF7BW125',
           airtime: 46336000,
@@ -48,8 +48,8 @@ module.exports = {
             '[{"gtw_id":"eui-30aea4fffe2cf768","timestamp":3544350807,"time":"2019-07-30T09:20:33.799332Z","channel":0,"rssi":-20,"snr":6,"rf_chain":0,"latitude":44.434933,"longitude":26.04587,"altitude":15,"location_source":"registry"}]',
           humidity: 0.95,
           temperature: 24,
-          createdAt: new Date().toDateString(),
-          updatedAt: new Date().toDateString()
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           hardware_serial: '70B3D5499243F193',
@@ -57,7 +57,7 @@ module.exports = {
           counter: 6,
           payload_raw: ' .',
           time: '2019-07-30T09:22:21.851553674Z',
-          frequency: 868100,
+          frequency: 868.1,
           modulation: 'LORA',
           data_rate: 'SF7BW125',
           airtime: 46336000,
@@ -66,8 +66,8 @@ module.exports = {
             '[{"gtw_id":"eui-30aea4fffe2cf768","timestamp":3652336572,"time":"2019-07-30T09:22:21.785326Z","channel":0,"rssi":-18,"snr":6,"rf_chain":0,"latitude":44.434933,"longitude":26.04587,"altitude":15,"location_source":"registry"}]',
           humidity: 0.46,
           temperature: 23,
-          createdAt: new Date().toDateString(),
-          updatedAt: new Date().toDateString()
+          createdAt: new Date(),
+          updatedAt: new Date()
         },
         {
           hardware_serial: '70B3D5499243F193',
@@ -75,7 +75,7 @@ module.exports = {
           counter: 9,
           payload_raw: ' 0',
           time: '2019-07-30T09:23:26.642579451Z',
-          frequency: 868100,
+          frequency: 868.1,
           modulation: 'LORA',
           data_rate: 'SF7BW125',
           airtime: 46336000,
@@ -84,8 +84,8 @@ module.exports = {
             '[{"gtw_id":"eui-30aea4fffe2cf768","timestamp":3717126435,"time":"2019-07-30T09:23:26.575442Z","channel":0,"rssi":-20,"snr":6,"rf_chain":0,"latitude":44.434933,"longitude":26.04587,"altitude":15,"location_source":"registry"}]',
           humidity: 0.48,
           temperature: 23,
-          createdAt: new Date().toDateString(),
-          updatedAt: new Date().toDateString()
+          createdAt: new Date(),
+          updatedAt: new Date()
         }
       ],
       {}
